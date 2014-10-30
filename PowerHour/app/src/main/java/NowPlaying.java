90c1ccbeb27745c308a0b70dcec02ece8febd75c
James Inbody <james@smart-temps.com>
2014-10-30 12:25:53 -0400

    Added method to get the name of the song playing

--- PowerHour/app/src/main/java/NowPlaying.java
+++ PowerHour/app/src/main/java/NowPlaying.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  * Created by James on 10/30/2014.
  *
  * Class to provide details of the currently playing song
  */
 public class NowPlaying {
+
+    private ArrayList<Object> currentList = new ArrayList<Object>();
+    private String mName;
+
+    public NowPlaying(String name){
+        currentList.add(mName);
+    }
+
+    public String getSongName() {
+        String title = currentList.get(0).toString();
+        return title;
+    }
 }
+
+
