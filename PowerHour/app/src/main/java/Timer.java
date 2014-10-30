cf0b508c92fd21276276a6088328f53851da60aa
James Inbody <james@smart-temps.com>
2014-10-30 12:55:09 -0400

    Created method to restart the timer

---
+++ PowerHour/app/src/main/java/Timer.java
@@ -0,0 +1,12 @@
+/**
+ * Created by James on 10/30/2014.
+ */
+public class Timer {
+
+    private static long RESTART_TIME = 6000;
+    private static long TIME_LEFT_IN_SONG;
+
+    public static void setNewTimer() {
+        
+    }
+}
