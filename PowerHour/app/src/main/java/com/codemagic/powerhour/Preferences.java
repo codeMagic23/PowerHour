31e39c79a560ca38d1aaa6a544ae5071b95e18bf
James Inbody <james@smart-temps.com>
2014-10-30 13:26:38 -0400

    Created preferences class

---
+++ PowerHour/app/src/main/java/com/codemagic/powerhour/Preferences.java
@@ -0,0 +1,13 @@
+package com.codemagic.powerhour;
+
+import android.content.Context;
+import android.preference.Preference;
+
+/**
+ * Created by James on 10/30/2014.
+ */
+public class Preferences extends Preference {
+    public Preferences(Context context) {
+        super(context);
+    }
+}
