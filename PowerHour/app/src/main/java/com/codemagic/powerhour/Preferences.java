4014fbd0c8281ab156abfde621a8e1241b75f0a9
James Inbody <james@smart-temps.com>
2014-10-30 13:27:40 -0400

    Added comments for description of prefs class

--- PowerHour/app/src/main/java/com/codemagic/powerhour/Preferences.java
+++ PowerHour/app/src/main/java/com/codemagic/powerhour/Preferences.java
@@ -5,6 +5,10 @@ import android.preference.Preference;
 
 /**
  * Created by James on 10/30/2014.
+ *
+ * This class may store time preferences,
+ * music service prefs,
+ * themes, etc...
  */
 public class Preferences extends Preference {
     public Preferences(Context context) {
